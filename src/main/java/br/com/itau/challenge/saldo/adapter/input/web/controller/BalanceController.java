3221d75a744fab8f6d499c660f5bc8c97d881de3
Pedro Ananias <pedro.phoa1405@gmail.com>
2026-08-12 21:17:24 -0300

    chore: add logs for observability

--- src/main/java/br/com/itau/challenge/saldo/adapter/input/web/controller/BalanceController.java
+++ src/main/java/br/com/itau/challenge/saldo/adapter/input/web/controller/BalanceController.java
@@ -3,6 +3,8 @@ package br.com.itau.challenge.saldo.adapter.input.web.controller;
 import br.com.itau.challenge.saldo.adapter.input.web.dto.BalanceResponse;
 import br.com.itau.challenge.saldo.domain.model.AccountBalance;
 import br.com.itau.challenge.saldo.port.input.IAccountBalance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,7 @@ import java.util.UUID;
 @RestController
 public class BalanceController {
 
+    private static final Logger log = LoggerFactory.getLogger(BalanceController.class);
     private final IAccountBalance IAccountBalance;
 
     public BalanceController(IAccountBalance IAccountBalance) {
@@ -20,7 +23,9 @@ public class BalanceController {
 
     @GetMapping("/balances/{accountId}")
     public BalanceResponse getBalance(@PathVariable UUID accountId) {
+        log.info("Searching balances for {}", accountId);
         AccountBalance accountBalance = IAccountBalance.getBalance(accountId);
+        log.info("Account {} balance is {}", accountId,accountBalance);
         return BalanceResponse.from(accountBalance);
     }
 }
--- src/main/java/br/com/itau/challenge/saldo/adapter/input/web/handler/GlobalExceptionHandler.java
+++ src/main/java/br/com/itau/challenge/saldo/adapter/input/web/handler/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package br.com.itau.challenge.saldo.adapter.input.web.handler;
 
 import br.com.itau.challenge.saldo.domain.exception.AccountNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,8 +13,11 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(AccountNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleAccountNotFound(AccountNotFoundException e) {
+        log.error(e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(Map.of("message", e.getMessage()));
